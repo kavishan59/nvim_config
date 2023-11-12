a193ee384bcfa4a41cd39651c91597ad12e70668
kavish <tkavisha@student.42.fr>
2023-11-12 20:15:42 +0100

    uploaded

---
+++ lua/plugins/nvim-lspconfig.lua
@@ -0,0 +1,121 @@
+local on_attach = require("util.lsp").on_attach
+
+local config = function()
+	require("neoconf").setup({})
+	local cmp_nvim_lsp = require("cmp_nvim_lsp")
+
+	local lspconfig = require("lspconfig")
+
+	local signs = { Error = " ", Warn = " ", Hint = "ﴞ ", Info = " " }
+	for type, icon in pairs(signs) do
+		local hl = "DiagnosticSign" .. type
+		vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+	end
+
+	local capabilities = cmp_nvim_lsp.default_capabilities()
+
+	-- lua
+	lspconfig.lua_ls.setup({
+		capabilities = capabilities,
+		on_attach = on_attach,
+		settings = { -- custom settings for lua
+			Lua = {
+				-- make the language server recognize "vim" global
+				diagnostics = {
+					globals = { "vim" },
+				},
+				workspace = {
+					-- make language server aware of runtime files
+					library = {
+						[vim.fn.expand("$VIMRUNTIME/lua")] = true,
+						[vim.fn.stdpath("config") .. "/lua"] = true,
+					},
+				},
+			},
+		},
+	})
+	-- python
+	lspconfig.pyright.setup({
+		capabilities = capabilities,
+		on_attach = on_attach,
+		settings = {
+			pyright = {
+				disableOrganizeImports = false,
+				analysis = {
+					useLibraryCodeForTypes = true,
+					autoSearchPaths = true,
+					diagnosticMode = "workspace",
+					autoImportCompletions = true,
+				},
+			},
+		},
+	})
+
+	-- bash
+	lspconfig.bashls.setup({
+		-- capabilities = capabilities,
+		on_attach = on_attach,
+		filetypes = { "sh", "aliasrc" },
+	})
+
+	-- C
+	lspconfig.clangd.setup({
+		capabilities = capabilities,
+		on_attach = on_attach,
+		cmd = {
+			"clangd",
+			"--offset-encoding=utf-16",
+		},
+	})
+
+	local luacheck = require("efmls-configs.linters.luacheck")
+	local stylua = require("efmls-configs.formatters.stylua")
+
+	lspconfig.efm.setup({
+		filetypes = {
+			"lua",
+		},
+		init_options = {
+			documentFormatting = true,
+			documentRangeFormatting = true,
+			hover = true,
+			documentSymbol = true,
+			codeAction = true,
+			completion = true,
+		},
+		settings = {
+			languages = {
+				lua = { luacheck, stylua },
+			},
+		},
+	})
+
+	-- Format on Save
+	local lsp_fmt_group = vim.api.nvim_create_augroup("LspFormattingGroup", {})
+	vim.api.nvim_create_autocmd("BufWritePost", {
+		group = lsp_fmt_group,
+		callback = function()
+			local efm = vim.lsp.get_active_clients({ name = "efm" })
+
+			if vim.tbl_isempty(efm) then
+				return
+			end
+
+			vim.lsp.buf.format({ name = "efm" })
+		end,
+	})
+end
+
+return {
+	"neovim/nvim-lspconfig",
+	config = config,
+	lazy = false,
+	dependencies = {
+		"windwp/nvim-autopairs",
+		"williamboman/mason.nvim",
+		"creativenull/efmls-configs-nvim",
+		"hrsh7th/nvim-cmp",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-nvim-lsp",
+	},
+}
